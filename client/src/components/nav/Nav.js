e37d913e8c6c5561c919c7bfb3b8f5896bcde3ba
Owen Ripley <ojripley@gmail.com>
2020-01-24 14:09:17 -0500

    add menu button functionality

--- client/src/components/nav/Nav.js
+++ client/src/components/nav/Nav.js
@@ -1,29 +1,33 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import '../styles/Nav.scss';
 
 export default function Nav(props) {
 
   const [menuOpen, setMenuOpen] = useState(false);
-  const [menuBarClasses, setMenuBarClasses] = ('');
+  const [menuIconClasses, setMenuIconClasses] = useState('menu-icon');
 
   const toggleMenu = function() {
     setMenuOpen(!menuOpen);
-
-    if (menuOpen) {
-      setMenuBarClasses()
-    }
   }
+
+  useEffect(() => {
+    if (menuOpen) {
+      console.log('changing menu bars');
+      setMenuIconClasses('menu-icon menu-open');
+    } else {
+      setMenuIconClasses('menu-icon');
+    }
+  }, [menuOpen]);
 
   return(
     <div id={'nav'}>
-      <div id={'menu-icon'} onClick={toggleMenu}>
+      <div className={menuIconClasses} onClick={toggleMenu}>
         <div className={'menu-icon-bar'} id={'bar1'}></div> 
         <div className={'menu-icon-bar'} id={'bar2'}></div>
         <div className={'menu-icon-bar'} id={'bar3'}></div>
         <div className={'menu-icon-bar'} id={'bar4'}></div>
       </div>
-      {menuOpen && <p>the menu is open</p>}
     </div>
   );
 };
