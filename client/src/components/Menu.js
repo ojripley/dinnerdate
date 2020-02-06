cc056190f6a77cb6d6a421ad94f96eb1d510f0e4
Owen Ripley <ojripley@gmail.com>
2020-02-05 19:06:56 -0500

    fix menu positioning glitch

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -3,8 +3,10 @@ import React from 'react';
 export default function Menu(props) {
 
   return(
-    <div className={props.menuClasses}>
-      <p>mennuuu</p>
+    <div className={'menu-container'}>
+      <div className={props.menuClasses}>
+        <p>mennuuu</p>
+      </div>
     </div>
   );
 };
