7d1f783466862586c88082078e9071535dea280a
Owen Ripley <ojripley@gmail.com>
2020-02-19 13:45:43 -0500

    improve new meal adding

--- client/src/components/MealQuickAdd.js
+++ client/src/components/MealQuickAdd.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import './styles/MealQuickAdd.scss';
 
 export default function MealQuickAdd(props) {
 
   const [mealToAdd, setMealToAdd] = useState('');
+  const addMealFieldRef = useRef(null);
 
   const submitMealToAdd = function() {
-    console.log('adding...', mealToAdd);
-
     if (mealToAdd.length > 0) {      
       if (props.socket) {
         props.socket.emit('addMeal', {user: props.user, mealName: mealToAdd});
+        addMealFieldRef.current.value = '';
       }
     }
   };
@@ -25,8 +25,8 @@ export default function MealQuickAdd(props) {
 
   return(
     <div className={'meal-quick-add'}>
-      <textarea className={'add-meal-field'} onChange={event => setMealToAdd(event.target.value)} onKeyPress={handleKeyPress} placeholder={'Add a new meal!'}></textarea>
-      <button className={'add-meal-button'} onClick={submitMealToAdd} >Add Meal</button>
+      {setMealToAdd && <textarea ref={addMealFieldRef} className={'add-meal-field'} onChange={event => setMealToAdd(event.target.value)} onKeyPress={handleKeyPress} placeholder={'Add a new meal!'}></textarea>}
+      <button className={'add-meal-button'} onClick={submitMealToAdd}>Add Meal</button>
     </div>
   );
 };
